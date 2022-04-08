218ea1e00007debf16b12a25a7eb69aef43b1553
chandikt <tanyachandik@gmail.com>
2022-04-08 12:18:18 -0400

    commit

---
+++ assets/background-image.js
@@ -0,0 +1,30 @@
+
+
+// https://cdn.shopify.com/s/files/1/0573/4111/6468/files/image-36.png
+
+var imageArr = ['image-1', 'image-2', 'image-3', 'image-4','image-5','image-6','image-7','image-8', 'image-9','image-10', 'image-11', 'image-12','image-13', 'image-14', 'image-15','image-16','image-17','image-18', 'image-19', 'image-20', 'image-21'];
+let arrLength = imageArr.length;
+
+let imageIndex = randomIndex(arrLength);
+
+console.log(imageIndex);
+
+function randomIndex(arrLength) {
+
+    return (Math.floor(Math.random()*arrLength));
+}
+
+
+function loadBackground(image) {
+
+///images/image-1.jpg
+document.body.style.backgroundImage = `url(https://cdn.shopify.com/s/files/1/0573/4111/6468/files/${image}.png)`
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadBackground(imageArr[imageIndex]);
+});
+
+
+
